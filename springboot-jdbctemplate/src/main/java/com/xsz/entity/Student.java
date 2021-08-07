db4d13f8dfef7a854ef4e08a2450b8c601c0539b
bsea <yinyouhai@aliyun.com>
2021-08-07 16:53:08 +0800

    jdbctemplate

---
+++ springboot-jdbctemplate/src/main/java/com/xsz/entity/Student.java
@@ -0,0 +1,55 @@
+package com.xsz.entity;
+
+import lombok.ToString;
+
+import java.io.Serializable;
+@ToString
+public class Student {
+    private int id;
+    private String name;
+    private int sex;
+    private int age;
+
+    public Student() {
+
+    }
+
+    public Student(int id, String name, int sex, int age) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
