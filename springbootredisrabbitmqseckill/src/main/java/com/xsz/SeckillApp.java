25bf0f66a951b25aa64139a10569a1b8712aeeb8
bsea <yinyouhai@aliyun.com>
2021-10-04 22:56:41 +0800

    redis

---
+++ springbootredisrabbitmqseckill/src/main/java/com/xsz/SeckillApp.java
@@ -0,0 +1,13 @@
+package com.xsz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class SeckillApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApp.class, args);
+    }
+}
